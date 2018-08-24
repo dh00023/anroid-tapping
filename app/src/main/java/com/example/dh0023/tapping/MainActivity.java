5a9438d0cf735ec3697478e773d4b18373ea3903
dahye Jeong <wjdekgp1750@naver.com>
2018-08-24 13:59:16 +0900

    html webview

--- app/src/main/java/com/example/dh0023/tapping/MainActivity.java
+++ app/src/main/java/com/example/dh0023/tapping/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dh0023.tapping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.cjmall);
+        webView.loadUrl("file:///android_asset/index.html");
+//        file:///Users/dh0023/Desktop/tapping/index.html
     }
 }
