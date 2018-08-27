3c7dc5345c1b889deca5a0cfc9659a5c05810f29
dahye Jeong <wjdekgp1750@naver.com>
2018-08-27 09:50:19 +0900

    상품정보레이어

--- README.md
+++ README.md
@@ -26,7 +26,7 @@
 ```java
 package com.example.dh0023.tapping;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app인AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -71,3 +71,30 @@ manifiest에 인터넷 권한 설정해주기
 
 3. WebViewClient의 shouldOverrideUrlLoading 함수를 사용해 페이지 이동막기
 	- [참조페이지](http://cofs.tistory.com/186)
+
+4. 새로운 Activity 생성후  shouldOverrideUrlLoading에서 연결하기
+5. 상품 정보 뜨는 레이아웃 만들기
+	- [참조페이지](http://wimir-dev.tistory.com/11)
+
+
+**질문하기**
+layoutinflater를 이용해서 버튼을 클릭하면 상품정보레이어가 나타나도록 구현했다. 그런데 상품정보레이어에 있는 button의 event처리가 안된다.
+그래서 우선적으로 CameraActivity에서 구매창이 뜨도록 하는 버튼 이벤트 처리를 따로 구현해보았다.
+`webView.loadUrl("http://display.cjmall.com/m/item/49595372/itemOrderOption?isNeededInterface=true&channelCode=30001001");`
+
+1. 웹뷰를 위에 얹으면 아래의 글과 이미지가 사라진다. 이걸 어떻게 해야하나요? 새로운 뷰를 생성해서 그게 연결되도록 해야하나요?
+2. 의문점! url로 바로구매버튼을 눌렀을 경우 그 웹뷰 내부에서 바로 cjmall바로구매페이지로 이동하게 되는데 그럼 이건 `shouldOverrideUrlLoading`에서 새로운 Activity로 연결해서 뜨게 하는건가요?
+
+
+
+
+- [Display image from url](https://stackoverflow.com/questions/3118691/android-make-an-image-at-a-url-equal-to-imageviews-image)
+
+
+
+
+### 상품정보
+
+- [스톤헨지 귀걸이](http://display.cjmall.com/p/item/49595372?channelCode=30001001&k=%EC%8A%A4%ED%86%A4%ED%97%A8%EC%A7%80)
+
+
--- app/src/main/java/com/example/dh0023/tapping/CameraActivity.java
+++ app/src/main/java/com/example/dh0023/tapping/CameraActivity.java
@@ -1,13 +1,49 @@
 package com.example.dh0023.tapping;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class CameraActivity extends AppCompatActivity {
+    RelativeLayout container;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+    public void onBtnClicked(View view){
+        container = (RelativeLayout) findViewById(R.id.container);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.product_info,container,true);
+        final Button button = (Button)findViewById(R.id.button);
+        button.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    public void buyBtnClicked(View view){
+        WebView web = (WebView)findViewById(R.id.buyView);
+        web.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        web.loadUrl("http://display.cjmall.com/m/item/49595372/itemOrderOption?isNeededInterface=true&channelCode=30001001");
+        final Button button1 = (Button)findViewById(R.id.btnBuy);
+        button1.setVisibility(View.INVISIBLE);
+
+        final Button button2 = (Button)findViewById(R.id.btnItem);
+        button2.setVisibility(View.INVISIBLE);
     }
 }
